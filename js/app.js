1176ad21cc21ac16e79c5aa84d82feaaa3e7b951
Oleksandr Volskyi <136353538+OleksandrVolsky@users.noreply.github.com>
2026-03-03 09:46:47 +0200

    feat: implement password generation and UI update functions

---
+++ js/app.js
@@ -0,0 +1,47 @@
+// пароль і кнопка копіювати
+const passwordEl = document.getElementById("password");
+const copyBtn = document.getElementById("copyBtn");
+
+// лінія і кількість символів
+const lengthValueEl = document.getElementById("lengthValue");
+const lengthRangeEl = document.getElementById("lengthRange");
+
+// чекбокси
+const upperEl = document.getElementById("upper");
+const lowerEl = document.getElementById("lower");
+const numbersEl = document.getElementById("numbers");
+const symbolsEl = document.getElementById("symbols");
+
+// Бари
+const strengthTextEl = document.getElementById("strengthText");
+const bars = document.querySelectorAll(".bar");
+
+// кнопка генерувати
+const generateBtn = document.getElementById("generatorBtn");
+
+
+// функція генерації пароля
+function generatorPass() {
+
+}
+
+
+// функція рахує strength
+function calculateStrength() {
+
+}
+
+
+// функція оновлює UI
+function updateStrengthUI() {
+
+}
+
+
+// функція копіювання пароля
+function copyPass() {
+
+}
+
+
+
